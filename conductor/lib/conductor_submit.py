14a071e51f5016ceafa7e9f94472a286a016bd0e
Martin Rhoads <ermal14@gmail.com>
2015-05-27 15:45:21 -0700

    break out submitter into bin and lib

--- conductor/lib/conductor_submit.py
+++ conductor/lib/conductor_submit.py
@@ -9,9 +9,7 @@ import os
 import sys
 import time
 import json
-import argparse
 import itertools
-import getpass
 import imp
 
 try:
@@ -21,7 +19,7 @@ except ImportError, e:
 
 import conductor
 import conductor.setup
-from conductor.lib import common, file_utils, api_client
+from conductor.lib import common, file_utils, api_client, uploader
 from httplib2 import Http
 
 
@@ -44,13 +42,9 @@ class Submit():
         logger.debug("Consumed args")
         self.api_client = conductor.lib.api_client.ApiClient()
 
-    @classmethod
-    def from_commmand_line(cls):
-        args = cls._parseArgs()
-        return cls(vars(args))  # convert the Namespace object to a dictionary
 
     def consume_args(self, args):
-        self.raw_command = args.get('cmd')
+        self.raw_command = args.get('cmd') or ''
         self.user = args.get('user') or getpass.getuser()
         self.frames = args.get('frames')
         self.upload_dep = args.get('upload_dependent')
@@ -91,64 +85,6 @@ class Submit():
 
 
 
-    @classmethod
-    def _parseArgs(cls):
-        parser = argparse.ArgumentParser(description=cls.__doc__,
-                formatter_class=argparse.RawDescriptionHelpFormatter)
-        parser.add_argument("--cmd",
-            help="execute this command.",
-            type=str)
-        parser.add_argument("--frames",
-            help="frame range to execute over.",
-            type=str)
-        parser.add_argument("--user",
-            help="Username to submit as",
-            type=str,
-            default=getpass.getuser(),
-            required=False)
-        parser.add_argument("--output_path",
-            help="path to copy renders to",
-            type=str,
-            required=False)
-        parser.add_argument("--upload_file",
-            help="The path to an upload file",
-            type=str,
-            required=False)
-        parser.add_argument("--upload_paths",
-            help="Paths to upload",
-            nargs="*")  # TODO(lws): ensure that this return an empty list if not specified
-        parser.add_argument("--resource",
-            help="resource pool to submit jobs to, defaults to show name.",
-            type=str,
-            required=False)
-        parser.add_argument("--cores",
-            help="Number of cores that this job should run on",
-            type=int,
-            required=False)
-        parser.add_argument("--priority",
-            help="Set the priority of the submitted job. Default is 5",
-            type=str,
-            required=False)
-        parser.add_argument("--upload_dependent",
-            help="job id of another job that this should be upload dependent on.",
-            type=str,
-            required=False)
-        parser.add_argument("--upload_only",
-            help="Only upload the files, don't start the render",
-            action='store_true')
-        parser.add_argument("--force",
-            help="Do not check for existing uploads, force a new upload",
-            action='store_true')
-        parser.add_argument("--postcmd",
-            help="Run this command once the entire job is complete and downloaded",
-            type=str,
-            required=False)
-        parser.add_argument("--skip_time_check",
-            action='store_true',
-            default=False,
-            help="Don't perform a time check between local and cloud")
-
-        return parser.parse_args()
 
 
     def send_job(self, upload_files=None):
@@ -181,6 +117,7 @@ class Submit():
                                 'command':self.raw_command,
                                 'cores':self.cores})
 
+
             if self.priority:
                 submit_dict['priority'] = self.priority
             if self.upload_dep:
@@ -196,7 +133,7 @@ class Submit():
 
 
         # TODO: verify that the response request is valid
-        response_dict, response_code = make_request(uri_path="jobs/", data=json.dumps(submit_dict))
+        response_dict, response_code = self.api_client.make_request(uri_path="jobs/", data=json.dumps(submit_dict))
         return response_dict, response_code
 
 
@@ -242,8 +179,3 @@ class Submit():
 
 class BadArgumentError(ValueError):
     pass
-
-
-if __name__ == '__main__':
-    submission = Submit.from_commmand_line()
-    submission.main()
