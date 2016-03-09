1e2202e2fe630784f7a2a9cfb63008597e87e7ea
justinonstine <justin@conductorio.com>
2016-03-09 10:49:54 -0800

    derp

--- conductor/lib/conductor_submit.py
+++ conductor/lib/conductor_submit.py
@@ -130,7 +130,7 @@ class Submit():
 
         self.notify = { "emails": self.resolve_arg(args, 'notify', [], combine_config=True),
                         "slack": self.resolve_arg(args, 'slack_notify', [], combine_config=True)}
-        logger.debug("notify: %s", self.notify)
+        logger.debug("notify: %s", self.notify) 
 
         logger.debug("Consumed args")
 
