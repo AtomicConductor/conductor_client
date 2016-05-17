cf2436ac1508c1150d4089e306634003d70ab33b
RobertAtomic <robert@conductorio.com>
2016-05-16 19:43:25 -0700

    build DMG

--- installers/osx/build.sh
+++ installers/osx/build.sh
@@ -4,7 +4,15 @@ pushd $( dirname "${BASH_SOURCE[0]}" )
 mkdir -p build/Conductor.app/Contents/MacOS build/Conductor.app/Contents/Resources build/Library/LaunchAgents
 cp -r ../../bin ../../conductor ../../maya_shelf ../../nuke_menu ../../clarisse_shelf ./python build/Conductor.app/Contents/MacOS
 cp info.plist build/Conductor.app/Contents
+cp Conductor.icns build/Conductor/Contents/Resources
 cp com.conductorio.conductor.plist build/Library/LaunchAgents
+
+dd if=/dev/zero of=ConductorClient.dmg bs=1M count=64
+mkfs.hfsplus -v ConductorClient ConductorClient.dmg
+mkdir mnt
+sudo mount -o loop ConductorClient.dmg mnt
+cp -r build/Conductor.app mnt
+sudo umount mnt
 
 popd
 
