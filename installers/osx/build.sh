df2f3fbdafa42aeb2cd2e45db4154aa392a79198
RobertAtomic <robert@conductorio.com>
2016-05-16 20:02:13 -0700

    build DMG

--- installers/osx/build.sh
+++ installers/osx/build.sh
@@ -4,6 +4,7 @@ pushd $( dirname "${BASH_SOURCE[0]}" )
 mkdir -p build/Conductor.app/Contents/MacOS build/Conductor.app/Contents/Resources build/Library/LaunchAgents
 cp -r ../../bin ../../conductor ../../maya_shelf ../../nuke_menu ../../clarisse_shelf ./python build/Conductor.app/Contents/MacOS
 cp info.plist build/Conductor.app/Contents
+cp conductor_client build/Conductor.app/Contents/MacOS
 cp Conductor.icns build/Conductor.app/Contents/Resources
 cp com.conductorio.conductor.plist build/Library/LaunchAgents
 
