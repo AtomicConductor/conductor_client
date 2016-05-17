5d2208278c651ed796f9b8d00bdbf1da8a376d89
RobertAtomic <robert@conductorio.com>
2016-05-16 18:22:29 -0700

    fix relative paths in windows installer

--- installers/windows/build.sh
+++ installers/windows/build.sh
@@ -3,7 +3,7 @@ pushd $( dirname "${BASH_SOURCE[0]}" )
 PATH=$PATH:./nsis/bin
 
 mkdir -p ./Conductor
-cp -r ../bin ../conductor ../maya_shelf ../nuke_menu ../clarisse_shelf ./python ./Conductor
+cp -r ../../bin ../../conductor ../../maya_shelf ../../nuke_menu ../../clarisse_shelf ./python ./Conductor
 
 makensis ConductorClient.nsi
 
