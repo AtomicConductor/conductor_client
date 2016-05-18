2b33f5476db7ccc4ecdbb6ab34352464c3d9ea36
RobertAtomic <robert@conductorio.com>
2016-05-17 22:17:23 -0700

    add post install action to OS X install

--- installers/osx/build.sh
+++ installers/osx/build.sh
@@ -3,12 +3,14 @@ pushd $( dirname "${BASH_SOURCE[0]}" )
 mkdir -p build/flat/base.pkg build/flat/Resources/en.lproj
 mkdir -p build/root/Applications/Conductor.app/Contents/MacOS build/root/Applications/Conductor.app/Contents/Resources
 mkdir -p build/root/Library/LaunchAgents
+mkdir -p build/scripts
+
 cp -r ../../bin ../../conductor ../../maya_shelf ../../nuke_menu ../../clarisse_shelf ./python build/root/Applications/Conductor.app/Contents/MacOS
 cp info.plist build/root/Applications/Conductor.app/Contents
 cp conductor_client setenv build/root/Applications/Conductor.app/Contents/MacOS
 cp Conductor.icns build/root/Applications/Conductor.app/Contents/Resources
 cp com.conductorio.conductor.plist build/root/Library/LaunchAgents
-echo "/Applications/Conductor.app/Contents/MacOS/bin" > build/root/etc/paths.d/conductor
+cp postinstall build/scripts
 
 pushd build
 PKG_FILES=$(find root | wc -l)
@@ -16,11 +18,15 @@ PKG_DU=$(du -b -s root | cut -f1)
 cat << EOF > flat/base.pkg/PackageInfo
 <pkg-info format-version="2" identifier="com.conductorio.Conductor.base.pkg" version="1.0.0" install-location="/" auth="root">
   <payload installKBytes="$PKG_DU" numberOfFiles="$PKG_FILES"/>
+  <scripts>
+    <postinstall file="./postinstall"/>
+  </scripts>
 <bundle-version>
     <bundle id="com.conductorio.conductor" CFBundleIdentifier="com.conductorio.conductor" path="./Applications/Conductor.app" CFBundleVersion="1"/>
 </bundle-version>
 </pkg-info>
 EOF
+
 cat << EOF > flat/Distribution
 <?xml version="1.0" encoding="utf-8"?>
 <installer-script minSpecVersion="1.000000" authoringTool="com.apple.PackageMaker" authoringToolVersion="3.0.3" authoringToolBuild="174">
@@ -47,7 +53,9 @@ cat << EOF > flat/Distribution
     <pkg-ref id="com.conductorio.Conductor.base.pkg" installKBytes="$PKG_DU" version="1.0.0" auth="Root">#base.pkg</pkg-ref>
 </installer-script>
 EOF
+
 ( cd root && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/base.pkg/Payload
+( cd scripts && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/base.pkg/Scripts
 ../utils/mkbom -u 0 -g 80 root flat/base.pkg/Bom
 ( cd flat && ../../utils/xar --compression none -cf "../../ConductorClient.pkg" * )
 popd
