839cfb4b15a1a2c6fd3d3f74f32971a30f88f386
RobertAtomic <robert@conductorio.com>
2016-05-23 10:36:54 -0700

    windows version fix

--- installers/windows/build.sh
+++ installers/windows/build.sh
@@ -5,6 +5,6 @@ PATH=$PATH:./nsis/bin
 mkdir -p ./Conductor
 cp -r ../../bin ../../conductor ../../maya_shelf ../../nuke_menu ../../clarisse_shelf ./python ./conductor.bat ./Conductor
 
-makensis /DVERSION="${RELEASE_VERSION}" /DINSTALLER_NAME="conductor-${RELEASE_VERSION}" ConductorClient.nsi
+makensis -DVERSION="${RELEASE_VERSION}" -DINSTALLER_NAME="conductor-${RELEASE_VERSION}" ConductorClient.nsi
 
 popd
