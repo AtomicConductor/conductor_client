b167ee59e26945398d85199ac569769cb01b2225
RobertAtomic <robert@conductorio.com>
2016-05-27 15:07:37 -0700

    update build.sh

--- installers/build.sh
+++ installers/build.sh
@@ -2,16 +2,11 @@
 curl -s -o python.tgz "https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz"
 tar zxf python.tgz
 
-pushd conductor_client
-
 sed -i "s/#__version__=\"0.0.0\"/__version__=\"${RELEASE_VERSION}\"/" \
     conductor/__init__.py
 echo $payload > conductor/release.json
 
-./installers/windows/build.sh
-./installers/osx/build.sh
-./installers/rpm/build.sh
-./installers/debian/build.sh
-
-curl -s -u ${GITHUB_API_TOKEN} --data-binary @installers/rpm/conductor-${RELEASE_VERSION}-0.noarch.rpm -H "Content-Type:application/octet-stream" "${UPLOAD_URL}?name=conductor-${RELEASE_VERSION}-0.noarch.rpm"
-curl -s -u ${GITHUB_API_TOKEN} --data-binary @installers/debian/conductor_${RELEASE_VERSION:1}.deb -H "Content-Type:application/octet-stream" "${UPLOAD_URL}?name=conductor_${RELEASE_VERSION:1}.deb"
+./windows/build.sh
+./osx/build.sh
+./rpm/build.sh
+./debian/build.sh
