d6719573922b0fe823394b4d526956b2363b6df4
RobertAtomic <robert@conductorio.com>
2016-05-27 19:57:24 -0700

    fix docker paths

--- installers/rpm/build.sh
+++ installers/rpm/build.sh
@@ -30,12 +30,13 @@ for dist_ver in 5 6 7; do
       /root/build-python.sh
     
     mv build-${dist_ver}/BUILDROOT/${VERSION} build-${dist_ver}/BUILDROOT/conductor-${RELEASE_VERSION}-0.el${dist_ver}.x86_64
+
     pushd build-${dist_ver}
     rpmbuild --define "_topdir ${PWD}" \
          --define "_version ${RELEASE_VERSION}" \
          --define "_dist el${dist_ver}" \
          -bb SPECS/conductor.spec
-    
+
     #upload our asset to GitHub
     curl -s -u \
         ${GITHUB_API_TOKEN} \
@@ -44,4 +45,3 @@ for dist_ver in 5 6 7; do
         "${UPLOAD_URL}?name=conductor-${RELEASE_VERSION}-0.el${dist_ver}.x86_64.rpm"
     popd
 done
-
