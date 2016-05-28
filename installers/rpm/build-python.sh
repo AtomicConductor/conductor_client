ca8e94c602b2e2dc56eb6a3152f29f06a4d2e5f3
RobertAtomic <robert@conductorio.com>
2016-05-27 19:57:24 -0700

    fix docker paths

--- installers/rpm/build-python.sh
+++ installers/rpm/build-python.sh
@@ -10,7 +10,7 @@ yum install -y sqlite-devel \
                tk-devel \
                libdb-devel
 
-sleep 600
+
 /root/src/configure --prefix=/root/python && make && make install
 curl -O "https://bootstrap.pypa.io/get-pip.py"
 /root/python/bin/python get-pip.py
