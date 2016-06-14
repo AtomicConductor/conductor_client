5d7cc5c3c3037bb0b228a7f2502f5517c7d18c86
RobertAtomic <robert@conductorio.com>
2016-06-14 11:41:39 -0700

    fix XBMLANGPATH in installers

--- installers/rpm/conductor.sh
+++ installers/rpm/conductor.sh
@@ -3,7 +3,7 @@
 #conductor environment setup
 export PYTHONPATH=$PYTHONPATH:/opt/conductor/python/lib/python2.7/site-packages:/opt/conductor
 export MAYA_SHELF_PATH=$MAYA_SHELF_PATH:/opt/conductor/maya_shelf
-export XBMLANGPATH=$XBMLANGPATH:/opt/conductor/resources
+export XBMLANGPATH=$XBMLANGPATH:/opt/conductor/conductor/resources
 export NUKE_PATH=$NUKE_PATH:/opt/conductor/nuke_menu
 export CONDUCTOR_CONFIG=$HOME/.conductor/config.yml
 export PATH=$PATH:/opt/conductor/bin
