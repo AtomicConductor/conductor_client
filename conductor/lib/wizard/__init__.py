b6af09d67928f9e32fb4981010834064315b48f7
RobertAtomic <robert@conductorio.com>
2016-06-24 12:06:01 -0700

    Add finish screen to wizard

--- conductor/lib/wizard/__init__.py
+++ conductor/lib/wizard/__init__.py
@@ -6,7 +6,7 @@ def run():
     if not 'CONDUCTOR_CONFIG' in os.environ:
         raise RuntimeError("CONDUCTOR_CONFIG is not set in the environment. Please contact Conductor support for help configuring Conductor Client")
     
-    if webbrowser.open('http://localhost:8085/index.html', new=0, autoraise=True):
+    if webbrowser.open('http://localhost:8085/index.html', new=1, autoraise=True):
         server.run(port=8085)
     else:
         raise RuntimeError ("Unable to open web browser.  Please contact Conductor support for help configuring Conductor Client")
