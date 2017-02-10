5eb8cc55ed5f35a22690674a5cef1d0196512f20
justinonstine <justin@conductorio.com>
2017-02-09 16:09:21 -0800

    first pass at clarisse submitter

--- clarisse_shelf/clarisse_shelf.py
+++ clarisse_shelf/clarisse_shelf.py
@@ -1,4 +1,4 @@
-from conductor import submitter_clarisse
-reload(submitter_clarisse)
-sc = submitter_clarisse.ClarisseConductorSubmitter()
+from conductor import submitter_clarisse_old
+reload(submitter_clarisse_old)
+sc = submitter_clarisse_old.ClarisseConductorSubmitter()
 sc.runUi()
