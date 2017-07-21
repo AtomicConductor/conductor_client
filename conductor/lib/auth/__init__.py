e1ac1526dcd8f753f8f6d3c081d023cc2618f415
justinonstine <justin@conductorio.com>
2017-07-21 13:30:43 -0700

    more debugging, trying to catch an OS error

--- conductor/lib/auth/__init__.py
+++ conductor/lib/auth/__init__.py
@@ -1,8 +1,17 @@
 import webbrowser
 import server
+import logging
+
 
 def run(creds_file, base_url):
-    if webbrowser.open("%s/api/oauth_jwt?redirect_uri=http://localhost:8085/index.html&scope=user&response_type=client_token" % base_url, new=2, autoraise=True):
-        server.run(port=8085, creds_file=creds_file)
-    else:
-        raise RuntimeError("Unable to open web browser.  Please contact Conductor support for help configuring Conductor Client")
+    logging.debug("Base URL is %s" % base_url)
+    try:
+        if webbrowser.open("%s/api/oauth_jwt?redirect_uri=http://localhost:8085/index.html&scope=user&response_type=client_token" % base_url, new=2, autoraise=True):
+            server.run(port=8085, creds_file=creds_file)
+        else:
+            raise RuntimeError("Unable to open web browser.  Please contact Conductor support for help configuring Conductor Client")
+    except OSError, e:
+        if e.errno == 4:
+            pass
+        else:
+            raise e
