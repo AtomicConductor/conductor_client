58750aaf4963524151558758c348862f893df145
Edgar Flores <edgar@conductorio.com>
2017-07-20 17:24:58 -0700

    Timing out local server

--- conductor/lib/auth/__init__.py
+++ conductor/lib/auth/__init__.py
@@ -1,8 +1,9 @@
 import webbrowser
 import server
 
+
 def run(creds_file):
-    if webbrowser.open("https://dashboard.dev-conductortech.com/api/oauth_jwt?redirect_uri=http://localhost:8085/index.html&scope=user&response_type=client_token", new=1, autoraise=True):
+    if webbrowser.open("https://dashboard.dev-conductortech.com/api/oauth_jwt?redirect_uri=http://localhost:8085/index.html&scope=user&response_type=client_token", new=2, autoraise=True):
         server.run(port=8085, creds_file=creds_file)
     else:
-        raise RuntimeError ("Unable to open web browser.  Please contact Conductor support for help configuring Conductor Client")
+        raise RuntimeError("Unable to open web browser.  Please contact Conductor support for help configuring Conductor Client")
