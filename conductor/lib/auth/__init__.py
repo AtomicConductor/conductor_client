856f846d0123bdc58b0c88934f845bcbfac6351f
justinonstine <justin@conductorio.com>
2017-07-21 14:08:57 -0700

    removed unneeded try/except

--- conductor/lib/auth/__init__.py
+++ conductor/lib/auth/__init__.py
@@ -5,13 +5,7 @@ import logging
 
 def run(creds_file, base_url):
     logging.debug("Base URL is %s" % base_url)
-    try:
-        if webbrowser.open("%s/api/oauth_jwt?redirect_uri=http://localhost:8085/index.html&scope=user&response_type=client_token" % base_url, new=2, autoraise=True):
-            server.run(port=8085, creds_file=creds_file)
-        else:
-            raise RuntimeError("Unable to open web browser.  Please contact Conductor support for help configuring Conductor Client")
-    except OSError, e:
-        if e.errno == 4:
-            pass
-        else:
-            raise e
+    if webbrowser.open("%s/api/oauth_jwt?redirect_uri=http://localhost:8085/index.html&scope=user&response_type=client_token" % base_url, new=2, autoraise=True):
+        server.run(port=8085, creds_file=creds_file)
+    else:
+        raise RuntimeError("Unable to open web browser.  Please contact Conductor support for help configuring Conductor Client")
