a4a36f056b58b55f9bff01e31204964d32715d4d
easythrees <ravi@conductortech.com>
2018-02-14 10:36:31 -0800

    Fixing trailing whitespace found by codacy

--- conductor/submitter_nuke.py
+++ conductor/submitter_nuke.py
@@ -253,7 +253,7 @@ class NukeConductorSubmitter(submitter.ConductorSubmitter):
         # save their script before continuing with submission
         if not self.checkSaveBeforeSubmission():
             raise exceptions.UserCanceledError()
-			
+
         nuke_utils.save_current_nuke_script()
 
         # Get the write nodes that have been selected by the user (in the UI)
