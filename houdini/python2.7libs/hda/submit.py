9996dd497b1f7487341f9480962bbd83b2a80341
Julian Mann <julian.mann@gmail.com>
2018-02-15 00:40:09 -0500

    tidy up, comment out ct-59 stuff, wrap reloads in conditional for dev mode

--- houdini/python2.7libs/hda/submit.py
+++ houdini/python2.7libs/hda/submit.py
@@ -1,23 +1,25 @@
-import hou
+# import hou
 import render_source
 import projects
 
 
 def doit(node):
-    try:
-        if hou.hipFile.hasUnsavedChanges():
-            save_file_response = hou.ui.displayMessage(
-                "There are some unsaved changes. Do you want to save the file before "
-                "submitting to Conductor?", buttons=(
-                    "Yes", "No", "Cancel"), close_choice=2)
-            if save_file_response == 2:
-                return
-            if save_file_response == 0:
-                hou.hipFile.save()
-        hou.ui.displayMessage(text='Job submitted')
+    """Implement in CT-59"""
+    pass
+    # try:
+    #     if hou.hipFile.hasUnsavedChanges():
+    #         save_file_response = hou.ui.displayMessage(
+    #             "There are some unsaved changes. Do you want to save the file before "
+    #             "submitting to Conductor?", buttons=(
+    #                 "Yes", "No", "Cancel"), close_choice=2)
+    #         if save_file_response == 2:
+    #             return
+    #         if save_file_response == 0:
+    #             hou.hipFile.save()
+    #     hou.ui.displayMessage(text='Job submitted')
   
-    except Exception as e:
-        hou.ui.displayMessage(text=str(e), title='Job submission failed')
+    # except Exception as e:
+    #     hou.ui.displayMessage(text=str(e), title='Job submission failed')
 
 
 def _can_submit(node):
