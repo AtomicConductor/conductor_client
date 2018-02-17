bd3c6b5b3b822e0e5895c7602e816a90b639f169
Will McDonald <will@conductorio.com>
2018-02-17 08:21:49 -0800

    CT-135 adding logic to show GPU widgets based on V-Ray settings

--- conductor/lib/maya_utils.py
+++ conductor/lib/maya_utils.py
@@ -786,6 +786,22 @@ def get_render_settings_node(renderer_name, strict=True):
     return renderer_settings_gettr[renderer_name](strict=strict)
 
 
+def is_vray_gpu_enabled():
+    '''
+    Return True if a GPU mode is selected in V-Ray's render settings
+    Current potential values set on V-Ray's productionEngine attr are:
+    0 = CPU mode (False)
+    1 = OpenCL mode (True)
+    2 = CUDA mode (True)
+    '''
+    vray_node = get_vray_settings_node(strict=False)
+    if vray_node:
+        production_engine = cmds.getAttr("%s.productionEngine" % vray_node)
+        if production_engine > 0:
+            return True
+    return False
+
+
 def is_arnold_tx_enabled():
     '''
     Return True if the "Use Existing .tx Textures" option is enabled in Arnolds
