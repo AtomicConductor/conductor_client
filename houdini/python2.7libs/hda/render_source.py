1a59e85570cc9fde10e5decd939b67e32621ac8e
Julian Mann <julian.mann@gmail.com>
2018-02-23 21:10:45 -0500

    extracting frame specs and clumping to a module that doesn't know about houdini

--- houdini/python2.7libs/hda/render_source.py
+++ houdini/python2.7libs/hda/render_source.py
@@ -11,34 +11,34 @@ software section, or even deal with it automatically.
 import submit
 
 
-def get_render_node(node):
-    """Get connected render node."""
-    inputs = node.inputs()
-    if not inputs:
-        return None
-    return inputs[0]
-
-
-def _get_render_type(input_node):
+def _get_nice_render_type(input_type):
     """Display render type in the node header."""
-    if not input_node:
+    if not input_type:
         return "Please connect a source node"
-
-    node_type = input_node.type().name()
-
-    if node_type == 'ifd':
+    if input_type == 'ifd':
         return 'Mantra render'
-    if node_type == 'arnold':
+    if input_type == 'arnold':
         return 'Arnold render'
-    if node_type == 'rib':
+    if input_type == 'rib':
         return 'Prman render'
-    if node_type in ['output', 'filecache']:
+    if input_type in ['output', 'filecache']:
         return 'Simulation'
     return 'Unknown'
 
 
+def get_render_node(node):
+    """Get connected render node."""
+    inputs = node.inputs()
+    return inputs[0] if inputs else None
+
+
+def get_render_type(node):
+    input_node = get_render_node(node)
+    return input_node.type().name() if input_node else None
+
+
 def update_input_node(node):
     """Callback triggered every time a connection is made/broken."""
-    render_type = _get_render_type(get_render_node(node))
+    render_type = _get_nice_render_type(get_render_type(node))
     node.parm('render_type').set(render_type)
     submit.update_button_state(node)
