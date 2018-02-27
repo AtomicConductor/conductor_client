63a89f9fdc2653f90bfff804a74c5fa4f98533ce
Lawrence Schlosser <lschlosser@atomicfiction.com>
2018-02-26 18:25:08 -0800

    increased Nuke command's verbosity level to 2

--- conductor/submitter_nuke.py
+++ conductor/submitter_nuke.py
@@ -149,7 +149,7 @@ class NukeConductorSubmitter(submitter.ConductorSubmitter):
               "frames": "10-20x2"}]
         '''
 
-        cmd_template = "nuke-render %s %s -F %s-%sx%s %s"
+        cmd_template = "nuke-render -V 2 %s %s -F %s-%sx%s %s"
 
         write_nodes = self.extended_widget.getSelectedWriteNodes()
         write_nodes_args = "-X %s" % (",".join(write_nodes))
