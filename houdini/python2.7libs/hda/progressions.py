ab724a73e1fa9d7ff337770b49077f132a006043
Julian Mann <julian.mann@gmail.com>
2018-03-02 21:00:19 -0500

    adding packageIds, environment to dry run

--- houdini/python2.7libs/hda/progressions.py
+++ houdini/python2.7libs/hda/progressions.py
@@ -1,5 +1,5 @@
 
-def _should_add(element, progression):
+def _should_add(element, progression, max_size):
     """Should this element be added to this progression?
 
     Specifically, if the progression has 0 or 1 elements,
@@ -11,10 +11,12 @@ def _should_add(element, progression):
     """
     if len(progression) < 2:
         return True
+    if len(progression) == max_size:
+        return False
     return progression[1] - progression[0] == element - progression[-1]
 
 
-def create(iterable):
+def create(iterable, max_size=-1):
     """Split a sequence of integers into arithmetic progressions.
 
     This is not necessarily the most compact set of
@@ -27,12 +29,14 @@ def create(iterable):
     results = [[]]
     iterable = sorted(iterable)
 
+    if max_size == -1:
+        max_size = len(iterable)
     # add a sentinel to the end - see below
     iterable.append(-1)
     p = 0
     for element in iterable:
         # if not adding to current progression, create a new progression.
-        if not _should_add(element, results[p]):
+        if not _should_add(element, results[p], max_size):
             lastp = p
             p += 1
             results.append([])
@@ -55,11 +59,28 @@ def create(iterable):
     else:
         results[p] = results[p][:-1]
 
+    singles = [pr[0] for pr in results if len(pr) == 1]
+    # results contains at least one sequence, so process the singles
+    # recursively.
+    if (len(singles) < len(results)):
+        singles = create(singles, max_size)
+        longs = [
+            progression for progression in results if len(progression) > 1]
+        results = singles + longs
+        results.sort(key=lambda v: v[0])
+
     return results
 
 
-print create([1, 2, 3, 5, 7, 8, 11, 13, 15, 17])
+# print create([1, 10, 20, 30, 34, 35, 36, 37, 38, 40, 101, 201, 301])
 
-print create([10, 20, 23, 5, 7, 8, 9, 11, 13, 15, 17])
 
-# hrender -d <source> -f <clumpstart> <clumpend> -i <clumpstep> -take <take> -o /tmp/render_output/ $JOB/<hipbase>_<timestamp>.hip
+# print create([1, 2, 3, 5, 7, 8, 11, 13, 15, 17])
+
+# print create([10, 20, 23, 5, 7, 8, 9, 11, 13, 15, 17])
+
+# print create(xrange(1, 50), 6)
+
+
+# hrender -d <source> -f <clumpstart> <clumpend> -i <clumpstep> -take
+# <take> -o /tmp/render_output/ $JOB/<hipbase>_<timestamp>.hip
