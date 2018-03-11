8ebcb1f1c6b313fea019d604e66440d8d7198d06
Julian Mann <julian.mann@gmail.com>
2018-03-11 09:52:04 -0500

    irregular clumps now produce frame spec string consisting of arithmetic progressions

--- tests/test_expansion.py
+++ tests/test_expansion.py
@@ -40,8 +40,8 @@ class ExpanderTest(unittest.TestCase):
 
     def test_evaluate_bad_string_raises(self):
         exp = Expander(toka="foo", tokb="bar")
-        with self.assertRaises(ValueError):
-            exp.evaluate("Hello <tokc> --<tokb>--")
+        result = exp.evaluate("Hello <tokc> --<tokb>--")
+        self.assertEqual(result, "Hello <tokc> --bar--")
 
     def test_evaluate_list(self):
         exp = Expander(toka="foo", tokb="bar")
@@ -51,8 +51,8 @@ class ExpanderTest(unittest.TestCase):
 
     def test_evaluate_bad_list_raises(self):
         exp = Expander(toka="foo", tokb="bar")
-        with self.assertRaises(ValueError):
-            exp.evaluate(["Hello <toka>", "Goodbye <tokc>"])
+        result = exp.evaluate(["Hello <toka>", "Goodbye <tokc>"])
+        self.assertIn("Goodbye <tokc>", result)
 
     def test_evaluate_dict(self):
         exp = Expander(toka="foo", tokb="bar")
@@ -61,11 +61,11 @@ class ExpanderTest(unittest.TestCase):
         self.assertDictContainsSubset({"a": "Hello foo --bar--"}, result)
         self.assertEqual(len(result.keys()), 2)
 
-    def test_evaluate_bad_dict_raises(self):
+    def test_evaluate_bad_dict_leaves_token_in_place(self):
         exp = Expander(toka="foo", tokb="bar")
-        with self.assertRaises(ValueError):
-            exp.evaluate(
-                {"a": "Hello <toka> --<tokb>--", "b": "Goodbye <tokc>"})
+        result = exp.evaluate(
+            {"a": "Hello <toka> --<tokb>--", "b": "Goodbye <tokc>"})
+        self.assertDictContainsSubset({"b": "Goodbye <tokc>"}, result)
 
 
 if __name__ == '__main__':
