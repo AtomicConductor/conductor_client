6ced6ef66372e7a79b3c126d2234673a4fca5aba
Julian Mann <julian.mann@gmail.com>
2018-03-10 21:04:18 -0500

    package_env class and tests

--- houdini/python2.7libs/hda/software_data.py
+++ houdini/python2.7libs/hda/software_data.py
@@ -11,6 +11,8 @@ import json
 import re
 # from conductor.lib.api_client import request_software_packages
 from tests.mocks.api_client_mock import request_software_packages
+from package_environment import PackageEnvironment
+from conductor import CONFIG
 
 
 def remove_unreachable(paths):
@@ -271,51 +273,14 @@ class PackageTree(object):
         name = to_name(kw)
         return [p for p in all_paths if p.endswith(name)]
 
-#     def merge_environments(self, paths, base_env={}):
-#         env = dict(base_env
-#         for path in paths:
-#             _find_by_path(self._tree, path)
-
-
-# def merge_package_environments(packages, base_env=None):
-#     '''
-#     For the given conductor software packages, resolve and merge their environements
-#     int one single dictionary.
-
-#     Merge policies:
-#         append: appends values, separated by colons
-#         exclusive: indicates that
-#     '''
-#     env = dict(base_env or {})  # Make a copy of the dict. Don't want to alter original
-#     for package in packages:
-# #         logger.debug("package: %s", package)
-#         for env_variable in package.get("environment", []):
-#             name = env_variable["name"]
-#             value = env_variable["value"]
-#             merge_policy = env_variable["merge_policy"]
-
-#             ### APPEND ###
-#             if merge_policy == "append":
-#                 env[name] = ":".join([env[name], value]) if  env.get(name) else value
-
-#             ### EXCLUSIVE ###
-#             elif merge_policy == "exclusive":
-#                 if name in  env and env[name] != value:
-#                     raise Exception("Could not merge package environments due to "
-#                                     "difference in exclusive environment variable: %s "
-#                                     "(%s vs %s)\n"
-#                                     "Packages:\n\t%s" % (name,
-#                                                          env[name],
-#                                                          value,
-#                                                          "\n\t".join([pformat(p) for p in packages])))
-#                 env[name] = value
-
-#             else:
-#                 raise Exception("Got unexpected merge policy: %s" % merge_policy)
-#     return env
-
-
-
+    def get_environment(self, paths):
+        """Get merged environment from paths."""
+        config_environment = CONFIG.get("environment") or {}
+        package_env = PackageEnvironment(config_environment)
+        for path in paths:
+            package = _find_by_path(self._tree, path)
+            package_env.extend(package)
+        return package_env.env
 
     @property
     def tree(self):
