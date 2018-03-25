730abe6666528a31806bbaa79bc1abd34b3581bf
Julian Mann <julian.mann@gmail.com>
2018-03-25 01:09:17 -0500

    subclass Job and add factory for sim and image render separation

--- conductor/houdini/hda/job.py
+++ conductor/houdini/hda/job.py
@@ -5,6 +5,9 @@ import hou
 import json
 from conductor.houdini.lib import data_block
 from conductor.houdini.lib.sequence.clump import Clump
+from conductor.houdini.lib.sequence.sequence import Sequence
+
+
 from conductor.houdini.hda.task import Task
 from conductor.houdini.hda import (
     frame_spec_ui,
@@ -24,40 +27,32 @@ OUTPUT_DIR_PARMS = {
 class Job(object):
     """Prepare a Job."""
 
+    @staticmethod
+    def create(node, tokens, scene):
+        """Factory makes one of the Job subclasses."""
+        if driver_ui.is_simulation(node):
+            return SingleTaskJob(node, tokens, scene)
+        return ClumpedJob(node, tokens, scene)
+
     def __init__(self, node, parent_tokens, scene_file):
-
         self._node = node
-        self._sequence = frame_spec_ui.main_frame_sequence(node)
-
-        # will be none if not doing scout frames
-        self._scout_sequence = frame_spec_ui.resolved_scout_sequence(node)
-        self._instance = self._get_instance()
         self._source_node = driver_ui.get_driver_node(self._node)
         self._source_type = driver_ui.get_driver_type(self._node)
-
-        if not (self._source_node):
-            raise hou.InvalidInput(
-                "%s needs a connected source node." %
-                self._node.name())
-
+        self._get_instance()
+        self._get_sequence()
         self._dependencies = dependency_scan.fetch(self._sequence)
         self._dependencies.append(scene_file)
-
         self._package_ids = software_ui.get_chosen_ids(self._node)
         self._environment = software_ui.get_environment(self._node)
-
         self._task_command = self._node.parm("task_command").unexpandedString()
-
-        self._title = self._node.parm("job_title").eval()
-
-        self._output_directory = self._get_output_dir()
-
-        self._metadata = self._node.parm("metadata").eval()
 
         self._tokens = self._collect_tokens(parent_tokens)
 
-        self._tasks = []
+        self._title = self._node.parm("job_title").eval()
+        self._output_directory = self._get_output_dir()
+        self._metadata = self._node.parm("metadata").eval()
 
+        self._tasks = []
         for clump in self._sequence.clumps():
             task = Task(clump, self._task_command, self._tokens)
             self._tasks.append(task)
@@ -90,37 +85,7 @@ class Job(object):
                   int(cores) and it['flavor'] == flavor][0]
         result["preemptible"] = bool(self._node.parm('preemptible').eval())
         result["retries"] = self._node.parm("retries").eval()
-        return result
-
-    def _collect_tokens(self, parent_tokens):
-        """Tokens are string kv pairs used for substitutions."""
-
-        tokens = {}
-        sorted_frames = sorted(self._sequence._frames)
-        tokens["CT_LENGTH"] = str(len(self._sequence))
-        tokens["CT_SEQUENCE"] = str(Clump.create(iter(self._sequence)))
-        tokens["CT_SEQUENCEMIN"] = str(sorted_frames[0])
-        tokens["CT_SEQUENCEMAX"] = str(sorted_frames[-1])
-        tokens["CT_SCOUT"] = ",".join(
-            [str(x) for x in Clump.regular_clumps(self._scout_sequence or [])])
-        tokens["CT_CLUMPSIZE"] = str(self._sequence.clump_size)
-        tokens["CT_CLUMPCOUNT"] = str(self._sequence.clump_count())
-        tokens["CT_SCOUTCOUNT"] = str(len(self._scout_sequence or []))
-        tokens["CT_CORES"] = str(self._instance.get("cores"))
-        tokens["CT_FLAVOR"] = self._instance.get("flavor")
-        tokens["CT_INSTANCE"] = self._instance.get("description")
-        tokens["CT_PREEMPTIBLE"] = "0" if self._instance.get(
-            "preemptible") else "1"
-        tokens["CT_RETRIES"] = str(self._instance.get("retries", 0))
-        tokens["CT_JOB"] = self.node_name
-        tokens["CT_SOURCE"] = self.source_path
-        tokens["CT_TYPE"] = self.source_type
-
-        for token in tokens:
-            hou.putenv(token, tokens[token])
-        tokens.update(parent_tokens)
-
-        return tokens
+        self._instance = result
 
     def remote_args(self):
         result = {}
@@ -188,6 +153,190 @@ class Job(object):
     @property
     def tasks(self):
         return self._tasks
+
+    def _get_sequence(self):
+        raise NotImplementedError()
+
+    def _collect_tokens(self, parent_tokens):
+        raise NotImplementedError()
+
+
+class ClumpedJob(Job):
+
+    def __init__(self, node, parent_tokens, scene_file):
+
+        super(ClumpedJob, self).__init__(node, parent_tokens, scene_file)
+
+    def _get_sequence(self):
+        self._sequence = frame_spec_ui.main_frame_sequence(self._node)
+        self._scout_sequence = frame_spec_ui.resolved_scout_sequence(
+            self._node)
+
+    def _collect_tokens(self, parent_tokens):
+        tokens = {}
+        sorted_frames = sorted(self._sequence._frames)
+        tokens["CT_LENGTH"] = str(len(self._sequence))
+        tokens["CT_SEQUENCE"] = str(Clump.create(iter(self._sequence)))
+        tokens["CT_SEQUENCEMIN"] = str(sorted_frames[0])
+        tokens["CT_SEQUENCEMAX"] = str(sorted_frames[-1])
+        tokens["CT_SCOUT"] = ",".join(
+            [str(x) for x in Clump.regular_clumps(self._scout_sequence or [])])
+        tokens["CT_CLUMPSIZE"] = str(self._sequence.clump_size)
+        tokens["CT_CLUMPCOUNT"] = str(self._sequence.clump_count())
+        tokens["CT_SCOUTCOUNT"] = str(len(self._scout_sequence or []))
+        tokens["CT_CORES"] = str(self._instance.get("cores"))
+        tokens["CT_FLAVOR"] = self._instance.get("flavor")
+        tokens["CT_INSTANCE"] = self._instance.get("description")
+        tokens["CT_PREEMPTIBLE"] = "0" if self._instance.get(
+            "preemptible") else "1"
+        tokens["CT_RETRIES"] = str(self._instance.get("retries", 0))
+        tokens["CT_JOB"] = self.node_name
+        tokens["CT_SOURCE"] = self.source_path
+        tokens["CT_TYPE"] = self.source_type
+
+        for token in tokens:
+            hou.putenv(token, tokens[token])
+        tokens.update(parent_tokens)
+
+        return tokens
+
+
+class SingleTaskJob(Job):
+    def __init__(self, node, parent_tokens, scene_file):
+        super(SingleTaskJob, self).__init__(node, parent_tokens, scene_file)
+
+    def _get_sequence(self):
+        start, end, step = [
+            self._source_node.parm(parm).eval() for parm in [
+                'f1', 'f2', 'f3']
+        ]
+        self._sequence = Sequence.from_range(start, end, step=step)
+        self._sequence.clump_size = len(self._sequence)
+        self._scout_sequence = None
+
+    def _collect_tokens(self, parent_tokens):
+        tokens = {}
+        sorted_frames = sorted(self._sequence._frames)
+        tokens["CT_LENGTH"] = str(len(self._sequence))
+        tokens["CT_SEQUENCE"] = str(Clump.create(iter(self._sequence)))
+        tokens["CT_SEQUENCEMIN"] = str(sorted_frames[0])
+        tokens["CT_SEQUENCEMAX"] = str(sorted_frames[-1])
+        tokens["CT_CORES"] = str(self._instance.get("cores"))
+        tokens["CT_FLAVOR"] = self._instance.get("flavor")
+        tokens["CT_INSTANCE"] = self._instance.get("description")
+        tokens["CT_PREEMPTIBLE"] = "0" if self._instance.get(
+            "preemptible") else "1"
+        tokens["CT_RETRIES"] = str(self._instance.get("retries", 0))
+        tokens["CT_JOB"] = self.node_name
+        tokens["CT_SOURCE"] = self.source_path
+        tokens["CT_TYPE"] = self.source_type
+
+        for token in tokens:
+            hou.putenv(token, tokens[token])
+        tokens.update(parent_tokens)
+
+        return tokens
+
+# class JJob(object):
+#     """Prepare a Job."""
+
+#     def __init__(self, node, parent_tokens, scene_file):
+
+#         self._node = node
+#         self._sequence = frame_spec_ui.main_frame_sequence(node)
+
+#         # will be none if not doing scout frames
+#         self._scout_sequence = frame_spec_ui.resolved_scout_sequence(node)
+#         self._instance = self._get_instance()
+#         self._source_node = driver_ui.get_driver_node(self._node)
+#         self._source_type = driver_ui.get_driver_type(self._node)
+
+#         if not (self._source_node):
+#             raise hou.InvalidInput(
+#                 "%s needs a connected source node." %
+#                 self._node.name())
+
+#         self._dependencies = dependency_scan.fetch(self._sequence)
+#         self._dependencies.append(scene_file)
+
+#         self._package_ids = software_ui.get_chosen_ids(self._node)
+#         self._environment = software_ui.get_environment(self._node)
+
+#         self._task_command = self._node.parm("task_command").unexpandedString()
+
+#         self._title = self._node.parm("job_title").eval()
+
+#         self._output_directory = self._get_output_dir()
+
+#         self._metadata = self._node.parm("metadata").eval()
+
+#         self._tokens = self._collect_tokens(parent_tokens)
+
+#         self._tasks = []
+
+#         for clump in self._sequence.clumps():
+#             task = Task(clump, self._task_command, self._tokens)
+#             self._tasks.append(task)
+
+#     def _get_output_dir(self):
+#         result = os.path.join(hou.getenv("JOB"), "render")
+#         if self._node.parm('override_output_dir').eval():
+#             ov_dir = self._node.parm('output_directory').eval()
+#             if ov_dir:
+#                 result = ov_dir
+
+#         else:
+#             # try to get from the source node
+#             parm_name = OUTPUT_DIR_PARMS.get(self._source_type)
+#             if parm_name:
+#                 path = self._source_node.parm(parm_name).eval()
+#                 ov_dir = os.path.dirname(path)
+#                 if ov_dir:
+#                     result = ov_dir
+#         return result
+
+#     def _get_instance(self):
+#         flavor, cores = self._node.parm(
+#             'machine_type').eval().split("_")
+
+#         instance_types = data_block.ConductorDataBlock(
+#             product="houdini").instance_types()
+
+#         result = [it for it in instance_types if it['cores'] ==
+#                   int(cores) and it['flavor'] == flavor][0]
+#         result["preemptible"] = bool(self._node.parm('preemptible').eval())
+#         result["retries"] = self._node.parm("retries").eval()
+#         return result
+
+#     def _collect_tokens(self, parent_tokens):
+#         """Tokens are string kv pairs used for substitutions."""
+
+#         tokens = {}
+#         sorted_frames = sorted(self._sequence._frames)
+#         tokens["CT_LENGTH"] = str(len(self._sequence))
+#         tokens["CT_SEQUENCE"] = str(Clump.create(iter(self._sequence)))
+#         tokens["CT_SEQUENCEMIN"] = str(sorted_frames[0])
+#         tokens["CT_SEQUENCEMAX"] = str(sorted_frames[-1])
+#         tokens["CT_SCOUT"] = ",".join(
+#             [str(x) for x in Clump.regular_clumps(self._scout_sequence or [])])
+#         tokens["CT_CLUMPSIZE"] = str(self._sequence.clump_size)
+#         tokens["CT_CLUMPCOUNT"] = str(self._sequence.clump_count())
+#         tokens["CT_SCOUTCOUNT"] = str(len(self._scout_sequence or []))
+#         tokens["CT_CORES"] = str(self._instance.get("cores"))
+#         tokens["CT_FLAVOR"] = self._instance.get("flavor")
+#         tokens["CT_INSTANCE"] = self._instance.get("description")
+#         tokens["CT_PREEMPTIBLE"] = "0" if self._instance.get(
+#             "preemptible") else "1"
+#         tokens["CT_RETRIES"] = str(self._instance.get("retries", 0))
+#         tokens["CT_JOB"] = self.node_name
+#         tokens["CT_SOURCE"] = self.source_path
+#         tokens["CT_TYPE"] = self.source_type
+
+#         for token in tokens:
+#             hou.putenv(token, tokens[token])
+#         tokens.update(parent_tokens)
+
+#         return tokens
 
 
 # class ClumpedJob(Job):
