68cce1256a5ab57eb8e4724d45a440515a6b1f95
Julian Mann <julian.mann@gmail.com>
2018-07-05 16:56:24 +0200

    added HOUDINI_PATH vars for win and linux, and appended HOUDINI_PATH to houdini.env on osx

--- installers/rpm/conductor.sh
+++ installers/rpm/conductor.sh
@@ -4,6 +4,7 @@
 export PYTHONPATH=$PYTHONPATH:/opt/conductor/python/lib/python2.7/site-packages:/opt/conductor
 export MAYA_SHELF_PATH=$MAYA_SHELF_PATH:/opt/conductor/maya_shelf
 export XBMLANGPATH=$XBMLANGPATH:/opt/conductor/conductor/resources/%B
+export HOUDINI_PATH=$HOUDINI_PATH:/opt/conductor/houdini
 export NUKE_PATH=$NUKE_PATH:/opt/conductor/nuke_menu
 export CONDUCTOR_CONFIG=$HOME/.conductor/config.yml
 export PATH=$PATH:/opt/conductor/bin
