3bf2784ac3728c209529b291f814fbb5a6d8118e
Julian Mann <julian.mann@gmail.com>
2019-04-02 21:07:34 -0500

    tidy

--- conductor/__init__.py
+++ conductor/__init__.py
@@ -1,23 +1,24 @@
 import logging
 from conductor.lib import common, loggeria
 
-#The version string is updated by the build system.
-#Do not modify the following line.
-#__version__="0.0.0"
+# The version string is updated by the build system.
+# Do not modify the following line.
+# __version__="0.0.0"
 
-# Read the config yaml file upon module import
-
-# Adding this because it supresses logging handler 
-# failures in Clarisse. 
+# Adding basicConfig because it supresses logging handler
+# failures in Clarisse. It doesn't seem to affect the
+# conductor logger behaviour in other places.
 logging.basicConfig()
 
+# Read the config yaml file upon module import
 try:
     CONFIG = common.Config().config
 except ValueError:
     # wizard.run()
     CONFIG = common.Config().config
 
-# IF there is log level specified in config (which by default there should be), then set it for conductor's logger
+# IF there is log level specified in config (which by default there should
+# be), then set it for conductor's logger
 log_level = CONFIG.get("log_level")
 if log_level:
     loggeria.set_conductor_log_level(log_level)
