1d44d003cb645fe26ab76de6b266405d275bdc9c
Julian Mann <julian.mann@gmail.com>
2019-05-21 23:45:49 -0500

    picked split logging from CT-959

--- conductor/__init__.py
+++ conductor/__init__.py
@@ -1,24 +1,19 @@
-import logging
 from conductor.lib import common, loggeria
 
-# The version string is updated by the build system.
+# The version string is updated by the build system. (No it isn't)
 # Do not modify the following line.
 # __version__="0.0.0"
 
-# Adding basicConfig because it supresses logging handler
-# failures in Clarisse. It doesn't seem to affect the
-# conductor logger behaviour in other places.
-logging.basicConfig()
-
-# Read the config yaml file upon module import
+# Read the config yaml file upon module import.
 try:
     CONFIG = common.Config().config
 except ValueError:
-    # wizard.run()
     CONFIG = common.Config().config
 
-# IF there is log level specified in config (which by default there should
-# be), then set it for conductor's logger
+
+# Must setup logging before setting the level, otherwise we get an
+# annoying complaint about no handlers for logger conductor.
+loggeria.setup_conductor_logging()
 log_level = CONFIG.get("log_level")
 if log_level:
     loggeria.set_conductor_log_level(log_level)
