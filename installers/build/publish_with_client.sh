93c831ac22aa4e91d8da5fc306e750ee52e53ad7
Julian Mann <julian.mann@gmail.com>
2019-10-08 18:20:33 -0500

    change AWS BUCKET var in encrypted env (#263)
    copy `latest` version and add headers

--- installers/build/publish_with_client.sh
+++ installers/build/publish_with_client.sh
@@ -2,10 +2,19 @@
 
 # Deploy files to AWS
 version=$( cat /artifacts/build/dc/current-version.txt | cut -d" " -f1 )
+headers="--metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --acl public-read"
 
 bucket=s3://${AWS_S3_BUCKET_NAME}/conductor-desktop/
 cd /artifacts
-aws s3 cp ./conductor-v${version}.pkg ${bucket}
-aws s3 cp ./conductor-v${version}.exe ${bucket}
-aws s3 cp ./conductor-v${version}-0.el6.x86_64.rpm ${bucket}
-aws s3 cp ./conductor-v${version}-0.el7.x86_64.rpm ${bucket}
+
+
+aws s3 cp ./conductor-v${version}.pkg ${bucket} ${headers}
+aws s3 cp ./conductor-v${version}.exe ${bucket} ${headers}
+aws s3 cp ./conductor-v${version}-0.el6.x86_64.rpm ${bucket} ${headers}
+aws s3 cp ./conductor-v${version}-0.el7.x86_64.rpm ${bucket} ${headers}
+
+# Generate copies with static name: latest
+aws s3 cp ${bucket}conductor-v${version}.pkg ${bucket}conductor-latest.pkg ${headers}
+aws s3 cp ${bucket}conductor-v${version}.exe ${bucket}conductor-latest.exe ${headers}
+aws s3 cp ${bucket}conductor-v${version}-0.el6.x86_64.rpm ${bucket}conductor-latest-0.el6.x86_64.rpm ${headers}
+aws s3 cp ${bucket}conductor-v${version}-0.el7.x86_64.rpm ${bucket}conductor-latest-0.el7.x86_64.rpm ${headers}
