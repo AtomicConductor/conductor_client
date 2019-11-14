934943302b1e863e899def068f44725416624dd1
Julian Mann <julian.mann@gmail.com>
2019-11-13 23:37:04 -0500

    minor

--- conductor/clarisse/scripted_class/upload_ui.py
+++ conductor/clarisse/scripted_class/upload_ui.py
@@ -1,7 +1,5 @@
 """
 Responds to events in the  upload section of the ConductorJob attribute editor.
-
-Currently, when 
 """
 
 import ix
@@ -11,13 +9,12 @@ def handle_local_upload(obj, attr):
     """
     Responds to local_upload changes.
 
-    We must not clean up the render file if local_upload is false, 
-    because it cleans up before the upload daemon has had a chance 
+    We must not clean up the render file if local_upload is false,
+    because it cleans up before the upload daemon has had a chance
     to upload it.
 
     Args:
         obj (ConductorJob):
         attr (OfAttr): Attribute that changed.
     """
-    hide_cleanup = not attr.get_bool()
-    obj.get_attribute("clean_up_render_package").set_hidden(hide_cleanup)
+    obj.get_attribute("clean_up_render_package").set_hidden(not attr.get_bool())
