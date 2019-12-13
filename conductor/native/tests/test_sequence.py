1750895f23a13f70a733caf3f3632f1227fdb100
Julian Mann <julian.mann@gmail.com>
2019-12-13 11:43:27 -0500

    Sequence supports negative numbers

--- conductor/native/tests/test_sequence.py
+++ conductor/native/tests/test_sequence.py
@@ -6,15 +6,67 @@ import os
 import sys
 import unittest
 
-from conductor.native.lib.sequence import Progression, Sequence
+from conductor.native.lib.sequence import Progression, Sequence, _resolve_frames
 
 NATIVE_MODULE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if NATIVE_MODULE not in sys.path:
     sys.path.insert(0, NATIVE_MODULE)
 
 
-class SequenceFactoryTest(unittest.TestCase):
+class ResolveFramesTest(unittest.TestCase):
+    def test_single_integer(self):
+        s = _resolve_frames(1)
+        self.assertEqual(s[0], 1)
+        self.assertEqual(len(s), 1)
 
+    def test_integer_range_args(self):
+        s = _resolve_frames(1, 5)
+        self.assertEqual(s[0], 1)
+        self.assertEqual(s[4], 5)
+        self.assertEqual(len(s), 5)
+
+    def test_integer_range_step_args(self):
+        s = _resolve_frames(1, 5, 2)
+        self.assertEqual(s[0], 1)
+        self.assertEqual(s[2], 5)
+        self.assertEqual(len(s), 3)
+
+    def test_sort_integer_range_step_args(self):
+        s = _resolve_frames(5, 1, 2)
+        self.assertEqual(s[0], 1)
+        self.assertEqual(s[2], 5)
+        self.assertEqual(len(s), 3)
+
+    def negative_integer_args(self):
+        s = _resolve_frames(-5, -1, 2)
+        self.assertEqual(s[0], 5)
+        self.assertEqual(s[2], 1)
+        self.assertEqual(len(s), 3)
+
+    def test_array(self):
+        s = _resolve_frames([1, 2, 6, 4])
+        self.assertEqual(s[0], 1)
+        self.assertEqual(s[2], 4)
+        self.assertEqual(len(s), 4)
+
+    def test_single_number_spec(self):
+        s = _resolve_frames("3")
+        self.assertEqual(s[0], 3)
+        self.assertEqual(len(s), 1)
+
+    def test_range_spec(self):
+        s = _resolve_frames("1-3")
+        self.assertEqual(s[0], 1)
+        self.assertEqual(len(s), 3)
+
+    def test_range_step_spec(self):
+        s = _resolve_frames("1-5x2")
+        self.assertEqual(s[0], 1)
+        self.assertEqual(s[2], 5)
+        self.assertEqual(len(s), 3)
+
+
+class SequenceFactoryTest(unittest.TestCase):
     def test_single_number_is_progression(self):
         s = Sequence.create(1)
         self.assertIsInstance(s, Progression)
@@ -57,22 +109,9 @@ class SequenceFactoryTest(unittest.TestCase):
 
 
 class SequenceFactoryFailTest(unittest.TestCase):
-
-    def test_negative_number(self):
-        with self.assertRaises(ValueError):
-            Sequence.create(-1)
-
-    def test_negative_end(self):
-        with self.assertRaises(ValueError):
-            Sequence.create(1, -10, 1)
-
     def test_negative_step(self):
         with self.assertRaises(ValueError):
             Sequence.create(1, 10, -1)
-
-    def test_negative_end_str(self):
-        with self.assertRaises(ValueError):
-            Sequence.create("10--30")
 
     def test_bad_spec(self):
         with self.assertRaises(ValueError):
@@ -83,8 +122,7 @@ class SequenceFactoryFailTest(unittest.TestCase):
             Sequence.create("1-10xf")
 
 
-class StartEndStepValuesTest(unittest.TestCase):
-
+class StartEndStepIntsTest(unittest.TestCase):
     def test_create_from_start_only(self):
         s = Sequence.create(1)
         self.assertEqual(s.start, 1)
@@ -97,39 +135,76 @@ class StartEndStepValuesTest(unittest.TestCase):
         self.assertEqual(s.end, 5)
         self.assertEqual(s.step, 1)
 
-    def test_create_from_start_end_strings(self):
-        s = Sequence.create("1-5")
-        self.assertEqual(s.start, 1)
-        self.assertEqual(s.end, 5)
-        self.assertEqual(s.step, 1)
-
-    def test_create_from_start_end_strings_backwards(self):
-        s = Sequence.create("5-1")
-        self.assertEqual(s.start, 1)
-        self.assertEqual(s.end, 5)
-        self.assertEqual(s.step, 1)
-
     def test_create_with_step_ints(self):
         s = Sequence.create(1, 5, 2)
         self.assertEqual(s.start, 1)
         self.assertEqual(s.end, 5)
         self.assertEqual(s.step, 2)
 
-    def test_create_with_step_strings(self):
+    def test_create_with_negative_start(self):
+        s = Sequence.create(-1, 5, 2)
+        self.assertEqual(s.start, -1)
+        self.assertEqual(s.end, 5)
+        self.assertEqual(s.step, 2)
+
+    def test_create_with_negative_start_end(self):
+        s = Sequence.create(-5, -1, 2)
+        self.assertEqual(s.start, -5)
+        self.assertEqual(s.end, -1)
+        self.assertEqual(s.step, 2)
+
+    def test_create_swap_negative_start_end(self):
+        s = Sequence.create(-1, -5, 2)
+        self.assertEqual(s.start, -5)
+        self.assertEqual(s.end, -1)
+        self.assertEqual(s.step, 2)
+
+
+class StartEndStepSpecTest(unittest.TestCase):
+    def test_create_from_start_end_spec(self):
+        s = Sequence.create("1-5")
+        self.assertEqual(s.start, 1)
+        self.assertEqual(s.end, 5)
+        self.assertEqual(s.step, 1)
+
+    def test_create_from_start_end_spec_backwards(self):
+        s = Sequence.create("5-1")
+        self.assertEqual(s.start, 1)
+        self.assertEqual(s.end, 5)
+        self.assertEqual(s.step, 1)
+
+    def test_create_with_step_spec(self):
         s = Sequence.create("1-5x2")
         self.assertEqual(s.start, 1)
         self.assertEqual(s.end, 5)
         self.assertEqual(s.step, 2)
 
-    def test_create_with_start_string(self):
+    def test_create_with_start_spec(self):
         s = Sequence.create("5")
         self.assertEqual(s.start, 5)
         self.assertEqual(s.end, 5)
         self.assertEqual(s.step, 1)
 
+    def test_create_with_negative_start(self):
+        s = Sequence.create("-1-5x2")
+        self.assertEqual(s.start, -1)
+        self.assertEqual(s.end, 5)
+        self.assertEqual(s.step, 2)
+
+    def test_create_with_negative_start_end(self):
+        s = Sequence.create("-5--1x2")
+        self.assertEqual(s.start, -5)
+        self.assertEqual(s.end, -1)
+        self.assertEqual(s.step, 2)
+
+    def test_create_swap_negative_start_end(self):
+        s = Sequence.create("-1--5x2")
+        self.assertEqual(s.start, -5)
+        self.assertEqual(s.end, -1)
+        self.assertEqual(s.step, 2)
+
 
 class SequenceToStringTest(unittest.TestCase):
-
     def test_progression_single(self):
         s = Progression(1, 1, 1)
         self.assertEqual(str(s), "1")
@@ -160,7 +235,6 @@ class SequenceToStringTest(unittest.TestCase):
 
 
 class ChunksTest(unittest.TestCase):
-
     def test_no_chunk_size(self):
         s = Sequence.create("1-100")
         self.assertEqual(s.chunk_size, 100)
@@ -233,7 +307,6 @@ class ChunksTest(unittest.TestCase):
 
 
 class IntersectionTest(unittest.TestCase):
-
     def test_does_intersect(self):
         s = Sequence.create("1-10")
         i = s.intersection(range(5, 15))
@@ -246,7 +319,6 @@ class IntersectionTest(unittest.TestCase):
 
 
 class UnionTest(unittest.TestCase):
-
     def test_creates_union_from_range(self):
         s = Sequence.create("1-10")
         u = s.union(range(5, 15))
@@ -260,7 +332,6 @@ class UnionTest(unittest.TestCase):
 
 
 class SequenceValidator(unittest.TestCase):
-
     def test_is_valid_method_true(self):
         self.assertTrue(Sequence.is_valid_spec("12-26x3, 1,7,8,2,4-9"))
         self.assertTrue(Sequence.is_valid_spec("1"))
@@ -279,16 +350,13 @@ class SequenceValidator(unittest.TestCase):
 
 
 class SequenceIterator(unittest.TestCase):
-
     def test_iterator_sorted_no_dups(self):
         s = Sequence.create("1-10, 8-20x2, 19, 17")
-        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9,
-                    10, 12, 14, 16, 17, 18, 19, 20]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 17, 18, 19, 20]
         self.assertEqual(list(s), expected)
 
 
 class ProgressionsTest(unittest.TestCase):
-
     def test_empty(self):
         result = Progression.factory([])
         self.assertEqual(result, [])
@@ -349,7 +417,6 @@ class ProgressionsTest(unittest.TestCase):
 
 
 class PermutationsTest(unittest.TestCase):
-
     def test_one_substitution(self):
 
         template = "image.%(frame)04d.tif"
@@ -368,11 +435,7 @@ class PermutationsTest(unittest.TestCase):
 
     def test_three_substitutions(self):
         template = "image_%(uval)02d_%(vval)02d.%(frame)04d.tif"
-        kw = {
-            "uval": "1-2",
-            "vval": "1-2",
-            "frame": "10-11"
-        }
+        kw = {"uval": "1-2", "vval": "1-2", "frame": "10-11"}
         result = list(Sequence.permutations(template, **kw))
         self.assertIn("image_01_01.0010.tif", result)
         self.assertIn("image_02_02.0011.tif", result)
@@ -381,7 +444,6 @@ class PermutationsTest(unittest.TestCase):
 
 
 class OffsetTest(unittest.TestCase):
-
     def test_offset_positive_value(self):
         s = Sequence.create("1-10")
         s = s.offset(5)
@@ -392,10 +454,10 @@ class OffsetTest(unittest.TestCase):
         s = s.offset(-5)
         self.assertEqual(list(s), list(range(6, 16)))
 
-    def test_raises_if_result_negative(self):
+    def test_negative_result_offset(self):
         s = Sequence.create("1-10")
-        with self.assertRaises(ValueError):
-            s = s.offset(-3)
+        s = s.offset(-3)
+        self.assertEqual(list(s), list(range(-2, 8)))
 
     def test_offset_sequence_has_same_chunk_size(self):
         s = Sequence.create("1-10", chunk_size=3)
@@ -409,7 +471,6 @@ class OffsetTest(unittest.TestCase):
 
 
 class ExpandFilenameTest(unittest.TestCase):
-
     def test_expand_result_same_length(self):
         s = Sequence.create("8-12")
         template = "image.#.exr"
@@ -444,9 +505,7 @@ class ExpandFilenameTest(unittest.TestCase):
             s.expand(template)
 
 
-
 class ResolveDollarTimeVarsTest(unittest.TestCase):
-
     def test_resolve_one_file_no_padding(self):
         s = Sequence.create("1")
         template = "image.$F.exr"
@@ -468,7 +527,6 @@ class ResolveDollarTimeVarsTest(unittest.TestCase):
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0], "image.1.00001.01.exr")
 
-
     def test_resolve_one_template_to_many_filenames(self):
         s = Sequence.create("1-5")
         template = "image.$2F.exr"
@@ -479,14 +537,18 @@ class ResolveDollarTimeVarsTest(unittest.TestCase):
 
     def test_resolve_many_templates_to_many_filenames(self):
         s = Sequence.create("1-3")
-        templates = ["/folder_1/image.$2F.exr", "/folder_2/image.$2F.exr", "/folder_3/image.$2F.exr"]
+        templates = [
+            "/folder_1/image.$2F.exr",
+            "/folder_2/image.$2F.exr",
+            "/folder_3/image.$2F.exr",
+        ]
         result = s.expand_dollar_f(*templates)
         self.assertEqual(len(result), 3)
         self.assertIn("/folder_2/image.02.exr", result)
         self.assertIn("/folder_3/image.03.exr", result)
 
-class ResolveFormatTest(unittest.TestCase):
 
+class ResolveFormatTest(unittest.TestCase):
     def test_resolve_one_file_no_padding(self):
         s = Sequence.create("1")
         template = "image.{frame}.exr"
@@ -508,7 +570,6 @@ class ResolveFormatTest(unittest.TestCase):
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0], "image.1.00001.01.exr")
 
-
     def test_resolve_one_template_to_many_filenames(self):
         s = Sequence.create("1-5")
         template = "image.{frame:02d}.exr"
@@ -519,7 +580,11 @@ class ResolveFormatTest(unittest.TestCase):
 
     def test_resolve_many_templates_to_many_filenames(self):
         s = Sequence.create("1-3")
-        templates = ["/folder_1/image.{frame:02d}.exr", "/folder_2/image.{frame:02d}.exr", "/folder_3/image.{frame:02d}.exr"]
+        templates = [
+            "/folder_1/image.{frame:02d}.exr",
+            "/folder_2/image.{frame:02d}.exr",
+            "/folder_3/image.{frame:02d}.exr",
+        ]
         result = s.expand_dollar_f(*templates)
         self.assertEqual(len(result), 3)
         self.assertIn("/folder_2/image.02.exr", result)
@@ -527,30 +592,28 @@ class ResolveFormatTest(unittest.TestCase):
 
 
 class ToCustomSpecTest(unittest.TestCase):
-
-
     def test_spec_single_number(self):
-         s = Sequence.create( 10)
-         self.assertEqual(s.to(":","%",";"), "10")
+        s = Sequence.create(10)
+        self.assertEqual(s.to(":", "%", ";"), "10")
 
     def test_spec_range(self):
-         s = Sequence.create( 0, 10 )
-         self.assertEqual(s.to(":","%",";"), "0:10")
+        s = Sequence.create(0, 10)
+        self.assertEqual(s.to(":", "%", ";"), "0:10")
 
     def test_spec_step_range(self):
-         s = Sequence.create(1, 9, 2)
-         self.assertEqual(s.to(":","%",";"), "1:9%2")
+        s = Sequence.create(1, 9, 2)
+        self.assertEqual(s.to(":", "%", ";"), "1:9%2")
 
     def test_spec_complex(self):
-         s = Sequence.create( "1-10, 14, 20-48x4")
-         self.assertEqual(s.to(":","%",";"), "1:10;14;20:48%4")
+        s = Sequence.create("1-10, 14, 20-48x4")
+        self.assertEqual(s.to(":", "%", ";"), "1:10;14;20:48%4")
+
     def test_complex_add_spaces(self):
-         s = Sequence.create( "1-10, 14, 20-48x4")
-         self.assertEqual(s.to(":","%","; "), "1:10; 14; 20:48%4")
+        s = Sequence.create("1-10, 14, 20-48x4")
+        self.assertEqual(s.to(":", "%", "; "), "1:10; 14; 20:48%4")
 
 
 class SubsampleTest(unittest.TestCase):
-
     def test_counts_from_1_to_10(self):
         s = Sequence.create("1-10")
         ss = s.subsample(1)
@@ -588,5 +651,5 @@ class SubsampleTest(unittest.TestCase):
         self.assertEqual(list(ss), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
