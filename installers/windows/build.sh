c0ed83ae6228aabff0af302a13970bf765171a36
Julian Mann <julian.mann@gmail.com>
2020-01-15 14:15:18 -0500

    adds updated icon (#278)

--- installers/windows/build.sh
+++ installers/windows/build.sh
@@ -21,7 +21,7 @@ cp -r ${SRC_DIR}/bin \
       ${SRC_DIR}/nuke_menu \
       ${SRC_DIR}/installers/windows/python \
       ${SRC_DIR}/installers/windows/conductor.bat \
-      ${SRC_DIR}/installers/windows/conductor_white.ico \
+      ${SRC_DIR}/installers/windows/companion-icon.ico \
       ${BUILD_DIR}/Conductor
 
 cp -r ${SRC_DIR}/installers/windows/conductor_128.ico \
