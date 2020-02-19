24a53e2583963ffc623fce2ef2f0e355568bea5c
Jesse Lehrman <jesse@conductortech.com>
2020-02-19 16:15:45 -0500

    Updates the location of the conductor comapanion.exe

--- installers/certify_companion_binaries.sh
+++ installers/certify_companion_binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
     
 echo ${WINDOWS_INSTALLER_CERTIFICATE} | base64 -d - > /tmp/authenticode-certificate.p12
-
-mv /artifacts/build/windows/Conductor/conductor-companion.exe /artifacts/build/windows/Conductor/conductor-companion.unsigned.exe
-osslsigncode sign -pkcs12 /tmp/authenticode-certificate.p12 -pass ${WINDOWS_INSTALLER_CERTIFICATE_PWORD} -n "Conductor Client" -i "https://www.conductortech.com/" -in /artifacts/build/windows/Conductor/conductor-companion.unsigned.exe -out /artifacts/build/windows/Conductor/conductor-companion2.exe
-rm /artifacts/build/windows/Conductor/conductor-companion.unsigned.exe
+/artifacts/build/dc/win64/conductor-companion.exe 
+mv /artifacts/build/dc/win64/conductor-companion.exe /artifacts/build/dc/win64/conductor-companion.unsigned.exe
+osslsigncode sign -pkcs12 /tmp/authenticode-certificate.p12 -pass ${WINDOWS_INSTALLER_CERTIFICATE_PWORD} -n "Conductor Client" -i "https://www.conductortech.com/" -in /artifacts/build/dc/win64/conductor-companion.unsigned.exe -out /artifacts/build/dc/win64/conductor-companion.exe
+rm /artifacts/build/dc/win64/conductor-companion.unsigned.exe
