89c2403ab2c374c6bbe153db7c6c764bb2b5004b
Jesse Lehrman <jesse@conductortech.com>
2020-06-18 21:04:53 -0400

    Adds several new features and some cleanup.
    - Removes all the hard-coded values
    - Adds support for additional command arguments
    - Adds support for a post-task command
    - Adds support for a post-job command

--- conductor/__beta__/job/maya.py
+++ conductor/__beta__/job/maya.py
@@ -13,20 +13,14 @@ class MayaRenderJob(job.Job):
             self.cmd = "Render"
             self.render_layer = "defaultRenderLayer"
             self.scene_path = scene_path
-            self.project_path = project_path
-            
-            self.software_packages_ids = ["bcfe5df6e2361d77ca7d7b9da76e351b", "936dac0a489071942be623da35dd71fb"]
+            self.project_path = project_path            
             self.upload_paths.append(scene_path)
-            
-        def _get_environment(self):
-            return {"MAYA_PLUG_IN_PATH": "/opt/solidangle/arnold-maya/3/arnold-maya-maya2018-3.2.1-1./plug-ins", "MAYA_LOCATION": "/opt/autodesk/maya-io/2018/maya-io2018.SP6", "MAYA_LICENSE": "unlimited", "PYTHONPATH": "/opt/autodesk/maya-io/2018/maya-io2018.SP6/Conductor:/opt/solidangle/arnold-maya/3/arnold-maya-maya2018-3.2.1-1./scripts", "MAYA_DISABLE_CIP": "1", "MAYA_SCRIPT_PATH": "/opt/solidangle/arnold-maya/3/arnold-maya-maya2018-3.2.1-1./scripts", "solidangle_LICENSE": "4101@docker_host", "MAYA_RENDER_DESC_PATH": "/opt/solidangle/arnold-maya/3/arnold-maya-maya2018-3.2.1-1.", "PATH": "/opt/autodesk/maya-io/2018/maya-io2018.SP6/bin:/opt/solidangle/arnold-maya/3/arnold-maya-maya2018-3.2.1-1./bin", "ARNOLD_PLUGIN_PATH": "/opt/solidangle/arnold-maya/3/arnold-maya-maya2018-3.2.1-1./plug-ins", "LD_LIBRARY_PATH": "/opt/autodesk/maya-io/2018/maya-io2018.SP6/lib:/opt/autodesk/maya-io/2018/maya-io2018.SP6/plug-ins/xgen/lib:/opt/autodesk/maya-io/2018/maya-io2018.SP6/plug-ins/bifrost/lib"}
-        
+            self.additional_cmd_args = ""
+            self.post_task_cmd = ""
+            self.post_job_cmd = None
+ 
         def _get_task_data(self):
-            
-            {"frames": "1-2", "command": "Render  -s 1 -e 2 -b 1 -rl defaultRenderLayer -rd /tmp/render_output/ -proj \"C:/Users/jlehrman/Documents/maya/projects/conductor/\" \"/Users/jlehrman/Documents/maya/projects/conductor/simple_shapes.ma\""},
 
-            [u'Render', u'-s', u'5', u'-e', u'5', u'-b', u'1', u'-rl', u'defaultRenderLayer', u'-rd', u'/tmp/render_output/', u'-proj', u'"C:/Users/jlehrman/Documents/maya/projects/conductor/"', u'"C:/Users/jlehrman/Documents/maya/projects/conductor/simple_shapes.ma"']
-            
             LOG.debug("Using a chunk size of {}".format(self.chunk_size))
             
             task_data = []
@@ -47,9 +41,17 @@ class MayaRenderJob(job.Job):
                                 'render_layer': self.render_layer,
                                 'output_path': self.output_path,
                                 'project_path': self.project_path,
-                                'scene_path': self.scene_path} 
+                                'scene_path': self.scene_path,
+                                'extra_args': self.additional_cmd_args,
+                                'post_cmd': self.post_task_cmd}
                 
                 task_data.append({"frames": "{}-{}".format(start_frame, end_frame),
-                                  "command": "{cmd} -s {start_frame} -e {end_frame} -b {frame_step} -rl {render_layer} -rd {output_path} -proj {project_path} -r arnold -ai:lve 2 {scene_path}".format(**command_args)})
+                                  "command": "{cmd} -s {start_frame} -e {end_frame} -b {frame_step} -rl {render_layer} -rd {output_path} -proj {project_path} {extra_args} {scene_path} && {post_cmd}".format(**command_args)})
+                
+            if self.post_job_cmd is not None:
+                task_data.append({"frames": "999999", 
+                                  "command": self.post_job_cmd})
+                
+                self.scout_frames = ",".join([str(f) for f in frames])
                 
             return task_data
