442f808432d2cdcce0f65829aeb3942eda7cedde
Julian Mann <julian.mann@gmail.com>
2020-10-22 12:01:32 -0500

    remove clarisse plugin and native libraries

--- conductor/native/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Common code for native submitters.
-
-The code in conductor.native is intended to support GUI submitters that
-have been written as first class plugins to their host applications
-(clarisse, houdini), as opposed to patching panels into the QT
-submitter. As such, there is no DCC specific code and no GUI code in
-here.
-"""
